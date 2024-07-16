dc214cce43fe42c5207c0da8427fe6f5376230dd
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-16 18:21:58 +0530

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    internal class Program
+    {
+
+
+        // Define an enum with explicit integer values
+        //named constants
+        //they are stored in the form of integers
+        //they should be stored in diff file as Enum Class name
+        //enum is only for integer constants only
+     
+
+        static void Main(string[] args)
+        {
+            // Assign enum values and use them
+            Status currentStatus = Status.InProgress;
+            Console.WriteLine($"Current status: {currentStatus}");
+            
+            int value = (int)Status.InProgress;
+            Console.WriteLine($"InProgress Status value: {value}");
+
+            // Check enum value
+
+            if (currentStatus == Status.InProgress)
+            {
+                Console.WriteLine("Task is in progress.");
+            }
+           
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Enums/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    //enum  Status:double  //not allowed
+    enum Status  //here name of enum should be in pascal case
+    {
+        Pending = 1, //and constants should be in whole capital or small
+        InProgress = 2,
+        Completed = 3,
+        Canceled = 4
+    }
+}
